f54e2d55121da585a0b560065a466d9479f93b4e
tjb <2448787464@qq.com>
2021-04-05 18:20:58 +0800

    更新3

--- src/wugong/DuGujiujian.java
+++ src/wugong/DuGujiujian.java
@@ -5,6 +5,8 @@ public class DuGujiujian {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("1111");
+System.out.println("2222");
+System.out.println("333");
 	}
 
 }
